a1dc10bddcf7dcf5363faee6e67abb64afaaa8ab
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2024-03-07 18:32:41 +0530

    I have updated something.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,3 +7,20 @@
 //         navLinkEl.classList.add('active');
 //     }
 // });
+
+let button1 = document.querySelector(".btn1ForMainPage");
+        button1.addEventListener("click",()=>{
+            alert("Presently - Avilable on the YouTube Handle.");
+            document.body.style.backgroundColor= "white"
+        })
+
+let button2 = document.querySelector(".btn2ForMainPage");
+button2.addEventListener("click",()=>{
+    alert("You can explore everything after Registration.");
+    document.body.style.backgroundColor= "pink"
+})
+
+let search = document.querySelector(".searchJs");
+search.addEventListener("click",()=>{
+    alert("Search is not working right now. please can wait for it.");
+})
